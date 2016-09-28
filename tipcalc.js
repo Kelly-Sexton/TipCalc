e0d298f1600dfbec0d29b81e72f69ddfcc4ccdc2
Kelly-Sexton <Kelly.sexton@interapthq.com>
2016-09-28 16:24:49 -0400

    Wed afternoon tip calc

--- tipcalc.js
+++ tipcalc.js
@@ -1,67 +1,70 @@
 $(function () {
 
-var bill = parseInt("#bill", 10);
+var bill = parseInt($("#bill"), 10);
 
-var people = parseInt("#people",10);
+var people = parseInt($("#people"),10);
 
-var service = "#service";
+var service = $("#service");
 
+// var bill = 20;//hardwired for debugging
 
+// var people = 4;//hardwired for debugging
 
+// var service = .05;//hardwire
 
+var tip = (bill * service); 
 
+var splitTip = ((bill / people) * service);
 
+var splitBill = (bill / people);
 
+var splitTotal = (splitBill + splitTip);
 
-
-
-
-
-
-
-
-
-
-
+var total = (bill + tip);
 
 
 
-
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(bill);
+console.log(tip);
+console.log(splitTip);
+console.log(splitBill);
+console.log(splitTotal);
+console.log(total);
 
 
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
