10ccfafd06c94a7833f0f3d75e91a5eee5092634
Kelly-Sexton <Kelly.sexton@interapthq.com>
2016-09-29 07:46:57 -0400

    morning push

--- tipcalc.js
+++ tipcalc.js
@@ -1,70 +1,16 @@
 $(function () {
-
-var bill = parseInt($("#bill"), 10);
-
-var people = parseInt($("#people"),10);
-
-var service = $("#service");
-
-// var bill = 20;//hardwired for debugging
-
-// var people = 4;//hardwired for debugging
-
-// var service = .05;//hardwire
-
-var tip = (bill * service); 
-
-var splitTip = ((bill / people) * service);
-
-var splitBill = (bill / people);
-
-var splitTotal = (splitBill + splitTip);
-
-var total = (bill + tip);
-
-
-
-console.log(bill);
-console.log(tip);
-console.log(splitTip);
-console.log(splitBill);
-console.log(splitTotal);
-console.log(total);
-
-
-
+$("#submit").click(function(){
+var prebill = $( "#bill" ).val();
+var people = $( "#people" ).val();
+// var service = $("#service").change();
+var service = "3";
+var splitBill = (((parseInt(prebill, 10)*100) / parseInt(people, 10)) / 100);
+var splitTip = (((parseInt(prebill, 10)*100) * parseInt(service, 10)) / (parseInt(people, 10))/1000); var splitTotal = (splitBill + splitTip);
+var tip = "Tip $";
+var bill = "Bill $";
+var total = "Total $";
+$( "#showTip" ).html( "<h2>" + tip + splitTip.toFixed(2) + "</h2>" )
+$( "#showBill" ).html( "<h2>" + bill + splitBill.toFixed(2) + "</h2>" )
+$( "#showTotal" ).html( "<h2>" + total + splitTotal.toFixed(2) + "</h2>" )
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+})
